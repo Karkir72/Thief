41aa98cdf5b246b962149451ea2ad4287c734230
Karabaza <karkir72@ya.ru>
2022-03-18 17:40:43 +0300

    Update

--- Scripts/Bell.cs
+++ Scripts/Bell.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Bell : MonoBehaviour
 {
-    private AudioSource _alarm;
     private Animator _animator;
+    private AudioSource _alarm;
 
     private void Start()
     {
